bcee50da77cc8b54334494e69f418724d1d99fc0
Maryia Rachkouskaya <maryia_rachkouskaya@epam.com>
2023-02-26 17:42:17 +0200

    feat: add getProductsList and getProductsById functions

---
+++ product-service/mocks.js
@@ -0,0 +1,7 @@
+module.exports.mockProducts = [
+  { id: '1', title: 'To Kill a Mockingbird', price: 9.99 },
+  { id: '2', title: 'Jane Eyre', price: 12.99 },
+  { id: '3', title: 'Sense and Sensibility', price: 12.99 },
+  { id: '4', title: 'Pride and Prejudice', price: 11.99 },
+  { id: '5', title: 'The Picture of Dorian Gray', price: 10.99 },
+];
